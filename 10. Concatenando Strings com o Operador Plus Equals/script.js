dc1346aa649a1724a6b56a9c6077e368d351a949
Eduardo Jahen <140217272+EduardoJahen@users.noreply.github.com>
2023-12-07 16:40:31 -0300

    Adding a new documents

---
+++ 10. Concatenando Strings com o Operador Plus Equals/script.js
@@ -0,0 +1,6 @@
+//Concatenando Strings com o Operador Plus Equals Também podemos usar o +=operador para concatenar uma string no final de uma variável de string existente. Isso pode ser muito útil para quebrar uma longa sequência em várias linhas.
+//Nota: Cuidado com os espaços. A concatenação não adiciona espaços entre strings concatenadas, então você mesmo precisará adicioná-las.
+
+let myStr = "This is the first sentence. "
+myStr += "This is the second sentence."
+console.log(myStr)
---
+++ 11. Construindo Strings com Variáveis/script.js
@@ -0,0 +1,6 @@
+// Only change code below this line
+//Às vezes você precisará construir uma string. Usando o operador de concatenação ( +), você pode inserir uma ou mais variáveis ​​em uma string que está construindo.
+
+const myName = "Eduardo";
+const myStr = "My name is " + myName + " and i am well!";
+console.log(myStr)
