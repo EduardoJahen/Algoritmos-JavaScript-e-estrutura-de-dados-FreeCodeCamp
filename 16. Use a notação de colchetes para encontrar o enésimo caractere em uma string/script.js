68285e624039d6ae9c79fb5401485290b6b1558d
Eduardo Jahen <140217272+EduardoJahen@users.noreply.github.com>
2023-12-10 10:43:30 -0300

    Add files via upload

---
+++ 16. Use a notação de colchetes para encontrar o enésimo caractere em uma string/script.js
@@ -0,0 +1,16 @@
+//Você também pode usar a notação de colchetes para colocar o caractere em outras posições dentro de uma string.
+
+//Lembre-se de que os computadores começam a contar em 0, então o primeiro caractere é na verdade o zero.
+//Ex: const firstName = "Ada";
+//const secondLetterOfFirstName = firstName[1];
+
+//secondLetterOfFirstNameteria um valor da string d.
+
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+console.log(thirdLetterOfLastName)
